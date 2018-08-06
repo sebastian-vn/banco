cb7ba5a4ece04eb7840cf5f94917bd1fb22e390b
sebastian-vn <37372300+sebastian-vn@users.noreply.github.com>
2018-08-06 09:53:09 -0500

    Add files via upload

--- js/contenido.js
+++ js/contenido.js
@@ -3,11 +3,6 @@ $(function(){
   
 });
 
-/* function mainsAdjust(){
-  $('#main_generalidades').css('padding-left', $('.sidenav').width());
-  $('#main_generalidades').css('padding-top', $('#nav').height());  
-} */
-
 function load(){
   $('body').addClass('animated fadeIn');
 }
